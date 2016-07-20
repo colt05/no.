6033e3464ecd7ec0005237d1dd687fa539da8c19
colt05-usr <colt05-usr@users.noreply.github.com>
2016-07-19 21:15:30 -0400

    game 1 not obfuscated

--- not_obfuscated/game.js
+++ not_obfuscated/game.js
@@ -4,7 +4,7 @@ function doNotRunThisCode(please){
   $=~[];$={___:++$,$$$$:(![]+"")[$],__$:++$,$_$_:(![]+"")[$],_$_:++$,$_$$:({}+"")[$],$$_$:($[$]+"")[$],_$$:++$,$$$_:(!""+"")[$],$__:++$,$_$:++$,$$__:({}+"")[$],$$_:++$,$$$:++$,$___:++$,$__$:++$};$.$_=($.$_=$+"")[$.$_$]+($._$=$.$_[$.__$])+($.$$=($.$+"")[$.__$])+((!$)+"")[$._$$]+($.__=$.$_[$.$$_])+($.$=(!""+"")[$.__$])+($._=(!""+"")[$._$_])+$.$_[$.$_$]+$.__+$._$+$.$;$.$$=$.$+(!""+"")[$._$$]+$.__+$._+$.$+$.$$;$.$=($.___)[$.$_][$.$_];$.$($.$($.$$+"\""+$.$_$_+(![]+"")[$._$_]+$.$$$_+"\\"+$.__$+$.$$_+$._$_+$.__+"(\\\"-.-\\\")"+"\"")())();
 }
 
-function verify(_key) {
+function verify(_key, next) {
   triplesec.decrypt ({
     data:          new triplesec.Buffer(ciphertext, "hex"),
     key:           new triplesec.Buffer(_key),
@@ -16,5 +16,10 @@ function verify(_key) {
       alert("Error! Did you input the correct password?");
     }
 });
+//the key and code are the same
 var plaintext = buff.toString();
+if (md5(plaintext) == "1f15c8645d67ef61676436dc1568cf2f") {
+  alert("correct");
+  next();
+}
 }
