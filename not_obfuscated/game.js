0d9fe8b0933574db56d2b7b2b4ddd367ae6f0cc8
colt05-usr <colt05-usr@users.noreply.github.com>
2016-07-30 14:30:17 -0400

    Update game.js

--- not_obfuscated/game.js
+++ not_obfuscated/game.js
@@ -1,3 +1,8 @@
+// Settings //
+settings = {};
+settings.ciphertext = "1c94d7de000000034676801a0be8e2cc6de22dd5a294b5e078530282dbeeee2c727fc7a3b4446e1a474faf00e5097c09f4adc4d7dec438d612167b102ebc030a650d17dd1f8dd5f2e5426d8d0b4029b19681b2be8cac2c531e5b22f413b80895e0bbbded5f2c775385cab2501bd52473d35096456fcb1196e182149d554cd7cb704b2696b3116b4507e5bfd59f6e49e56158dede89b47a6fdbbea23096513bb87078cd48eba74aa9b579cc58f7eb71e77d4d48bcd3a30c4d11d2f3fcecc4d6e5599e1bcedbb072df0ec2be21008893455902";
+//////////////
+
 // WARNING:
 // Some of this code is obfuscated.
 function doNotRunThisCode(please){
@@ -6,7 +11,7 @@ function doNotRunThisCode(please){
 
 function verify(_key, next) {
   triplesec.decrypt ({
-    data:          new triplesec.Buffer(ciphertext, "hex"),
+    data:          new triplesec.Buffer(settings.ciphertext, "hex"),
     key:           new triplesec.Buffer(_key),
     progress_hook: function (obj) {}
 }, function (err, buff) {
@@ -18,7 +23,7 @@ function verify(_key, next) {
 });
 //the key and code are the same
 var plaintext = buff.toString();
-if (md5(plaintext) == "1f15c8645d67ef61676436dc1568cf2f") {
+if (md5(plaintext).concat(_key) == "1f15c8645d67ef61676436dc1568cf2f") {
   alert("correct");
   next();
 }
